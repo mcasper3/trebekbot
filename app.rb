63d3c56188f03f2c1278c6460826639faaaf257a
Michael Casper <mcasper@us.ibm.com>
2016-08-01 09:05:06 -0500

    fixed currency format

--- app.rb
+++ app.rb
@@ -186,7 +186,7 @@ def process_answer(params)
       end
       mark_question_as_answered(params[:channel_id])
     elsif is_question_format?(user_answer) && is_correct_answer?(current_answer, user_answer)
-      reply = "That is correct, #{get_slack_name(user_id)}. Your total score is #{update_score(user_id, current_question["value"])}."
+      reply = "That is correct, #{get_slack_name(user_id)}. Your total score is #{currency_format(update_score(user_id, current_question["value"]))}."
       mark_question_as_answered(params[:channel_id])
     elsif is_correct_answer?(current_answer, user_answer)
       score = update_score(user_id, (current_question["value"] * -1))
