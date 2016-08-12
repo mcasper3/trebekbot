f78ac3da5eae1aee1f245f9d221b9d1fb5c007c9
Michael Casper <mcasper@us.ibm.com>
2016-08-12 10:04:57 -0500

    maybe fixed date

--- app.rb
+++ app.rb
@@ -102,7 +102,8 @@ def respond_with_question(params)
       previous_question = JSON.parse(previous_question)["answer"]
       question = "The answer is `#{previous_question}`.\n"
     end
-    question += "The category is `#{response["category"]["title"]}` for #{currency_format(response["value"])} (from `#{response["airdate"].strftime("%Y")}`): `#{response["question"]}`"
+    date = Date.parse(response["airdate"])
+    question += "The category is `#{response["category"]["title"]}` for #{currency_format(response["value"])} (from `#{date.strftime("%Y")}`): `#{response["question"]}`"
     puts "[LOG] ID: #{response["id"]} | Category: #{response["category"]["title"]} | Question: #{response["question"]} | Answer: #{response["answer"]} | Value: #{response["value"]}"
     $redis.pipelined do
       $redis.set(key, response.to_json)
